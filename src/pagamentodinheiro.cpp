89e3ae1a3738abadc3f03a8ca5bfcee1f4843b65
ITHALO-FS <ithalohenrique.dossantos@gmail.com>
2025-10-24 23:52:17 -0300

    Add files via upload

---
+++ src/pagamentodinheiro.cpp
@@ -0,0 +1,14 @@
+#include "pagamentodinheiro.h"
+#include <iostream>
+
+// p registrar pagamento
+void pagamentodinheiro::inserirdinheiro(double valor) {
+    if (valor <= 0) {
+        std::cout << "Valor inválido! Insira um valor positivo.\n";
+        return; //sai do metodo sem adicionar
+    }
+
+    valorpago += valor; //adiona valor 
+    std::cout << "Você inseriu R$ " << valor << " em dinheiro.\n"
+              << "Total inserido: R$ " << valorpago << "\n";
+}
